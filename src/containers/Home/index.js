4e6bd32c75cd08a880be5162a6e99763a6614d9f
Iamsindu <csitan.sindhu@gmail.com>
2022-02-13 17:06:31 +0545

    hero section done

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,19 +1,35 @@
 import React from "react";
-import { Col, Container, Row } from "react-bootstrap";
+import { Button, Col, Container, Row } from "react-bootstrap";
 import Footer from "../../components/Footer";
+import hero__img from "../../assets/images/hero__img.png";
 
 const index = () => {
   return (
     <>
       {/* hero-section */}
-      <div className="hero-section">
+      <section className="hero">
         <Container>
           <Row>
-            <Col lg={6}></Col>
-            <Col lg={6}></Col>
+            <Col lg={6}>
+              <div className="hero__content">
+                <h2 className="hero__content--heading mb-13">
+                  Get 10% off with Paicho Lemon Pickle{" "}
+                </h2>
+                <p className="hero__content--para mb-32">
+                  Organic Food, Good Health , Good Mood{" "}
+                </p>{" "}
+                <br />
+                <Button>Buy Now</Button>
+              </div>
+            </Col>
+            <Col lg={6}>
+              <div className="hero__banner">
+                <img src={hero__img} className="hero__banner--img" />
+              </div>
+            </Col>
           </Row>
         </Container>
-      </div>
+      </section>
       <Footer />
     </>
   );
