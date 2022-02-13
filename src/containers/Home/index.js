c621277eeb8aa0e8d515fb09e0e4b38c61db1bab
Iamsindu <csitan.sindhu@gmail.com>
2022-02-13 15:55:55 +0545

    footer classNames into BEM

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,9 +1,19 @@
 import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
 import Footer from "../../components/Footer";
 
 const index = () => {
   return (
     <>
+      {/* hero-section */}
+      <div className="hero-section">
+        <Container>
+          <Row>
+            <Col lg={6}></Col>
+            <Col lg={6}></Col>
+          </Row>
+        </Container>
+      </div>
       <Footer />
     </>
   );
