74a4ba86848c5b144bc8551207a04b5d8f873e29
Iamsindu <csitan.sindhu@gmail.com>
2022-02-13 10:36:20 +0545

    project set-up

---
+++ src/containers/App/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import Home from "../../containers/Home/index";
+
+const App = () => {
+  return (
+    <div>
+      <main>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+        </Routes>
+      </main>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/containers/Home/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const index = () => {
+  return <div>Lets start coding ..........</div>;
+};
+
+export default index;
