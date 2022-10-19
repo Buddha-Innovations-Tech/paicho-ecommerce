26e943a3652bb3fe9e034532aff43505edfab956
Akash Khattri <akash.khattri.ak@gmail.com>
2022-10-19 13:35:19 +0545

    sass fixes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./containers/App";
-import { BrowserRouter as Router } from "react-router-dom";
-import "../src/styles/App.scss";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import { Provider } from "react-redux";
-import store from "./store";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './containers/App';
+import { BrowserRouter as Router } from 'react-router-dom';
+import '../src/styles/App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import { Provider } from 'react-redux';
+import store from './store';
 
 ReactDOM.render(
   <Router>
@@ -15,5 +15,5 @@ ReactDOM.render(
       <App />
     </Provider>
   </Router>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
