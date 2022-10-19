ffdd177f149a254cd6f1d5383725285e9a8d1695
Akash Khattri <akash.khattri.ak@gmail.com>
2022-10-19 14:09:05 +0545

    sd

--- src/containers/OrderConfirmation/index.js
+++ src/containers/OrderConfirmation/index.js
@@ -1,60 +1,61 @@
-import { Col, Container, Row } from "react-bootstrap";
-import { CgChevronDoubleRight } from "react-icons/cg";
-import { Link } from "react-router-dom";
-import React from "react";
-import Order from "../../assets/images/order__confirm.png";
-import { useSelector, useDispatch } from "react-redux";
-import NavBar from "../../components/NavBar";
-import Footer from "../../components/Footer";
-import ScrollToTop from "../../components/ScrollToTop";
-import { Helmet } from "react-helmet";
+import { Col, Container, Row } from 'react-bootstrap';
+import { CgChevronDoubleRight } from 'react-icons/cg';
+import { Link } from 'react-router-dom';
+import React from 'react';
+import Order from '../../assets/Images/order__confirm.png';
+import { useSelector, useDispatch } from 'react-redux';
+import NavBar from '../../components/NavBar';
+import Footer from '../../components/Footer';
+import ScrollToTop from '../../components/ScrollToTop';
+import { Helmet } from 'react-helmet';
 
 const OrderConfirmation = () => {
   const { subscriberInfo } = useSelector((state) => state.subscriberLogin);
 
   return (
     <>
-    <ScrollToTop/>
-    <Helmet>
-      <title>Paicho-Order Confirm</title>
-    </Helmet>
-      <ScrollToTop/>
+      <ScrollToTop />
+      <Helmet>
+        <title>Paicho-Order Confirm</title>
+      </Helmet>
+      <ScrollToTop />
       <NavBar />
-      <section className="order__confirm">
+      <section className='order__confirm'>
         <Container>
           <Row>
             <Col md={6}>
-              <div className="order__confirm--left">
-                <div className="order__confirm--left-text mt-4">
-                {subscriberInfo ? (
+              <div className='order__confirm--left'>
+                <div className='order__confirm--left-text mt-4'>
+                  {subscriberInfo ? (
                     <>
-                      <span className="greeting">
-                       Hey, {`${subscriberInfo.firstname} ${subscriberInfo.lastname}`}
+                      <span className='greeting'>
+                        Hey,{' '}
+                        {`${subscriberInfo.firstname} ${subscriberInfo.lastname}`}
                       </span>
                     </>
                   ) : (
-                    ""
+                    ''
                   )}
-                  <p className="my-14">Your Order is Complete !</p>
+                  <p className='my-14'>Your Order is Complete !</p>
                   {/* <span className="content">
                     You will be receiving a confirmation email/ code <br /> with
                     order details
                   </span> */}
                 </div>
-                <div className="order__confirm--left-shopmore">
+                <div className='order__confirm--left-shopmore'>
                   <span>Happy Shopping ! </span>
-                  <div className="mt-2">
-                    <Link to="/">
+                  <div className='mt-2'>
+                    <Link to='/'>
                       Shop More
-                      <CgChevronDoubleRight className="me-2" />
+                      <CgChevronDoubleRight className='me-2' />
                     </Link>
                   </div>
                 </div>
               </div>
             </Col>
             <Col md={6}>
-              <div className="order__confirm--right">
-                <img src={Order} alt="order" className="img-fluid" />
+              <div className='order__confirm--right'>
+                <img src={Order} alt='order' className='img-fluid' />
               </div>
             </Col>
           </Row>
