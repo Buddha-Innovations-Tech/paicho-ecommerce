6e23d355d81b3595491ae18e5a0940d73038316d
Akash Khattri <akash.khattri.ak@gmail.com>
2022-11-08 10:23:56 +0545

    mobile nav fix

--- src/components/SubNav/index.js
+++ src/components/SubNav/index.js
@@ -1,47 +1,52 @@
-import React, { useState,useEffect,useRef } from "react";
-import { MdKeyboardArrowRight } from "react-icons/md";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect, useRef } from 'react';
+import { MdKeyboardArrowRight } from 'react-icons/md';
+import { Link } from 'react-router-dom';
 
 const SubNav = ({ title, categories }) => {
   const [open, setOpen] = useState(false);
   const myRef = useRef();
 
-  const handleClickOutside = e => {
+  const handleClickOutside = (e) => {
     if (!myRef.current.contains(e.target)) {
-        setOpen(true);
+      setOpen(true);
     }
-};
+  };
 
-const handleClickInside = () => setOpen(false);
+  const handleClickInside = () => setOpen(false);
 
-useEffect(() => {
+  useEffect(() => {
     document.addEventListener('mousedown', handleClickOutside);
     return () => document.removeEventListener('mousedown', handleClickOutside);
-});
-    
+  });
+
   return (
     <>
-      <li className="navar-dropdown small-navitem">
-        <Link to={`/${title}`}>{title}</Link>
+      <li className='navar-dropdown small-navitem'>
+        <Link to={`/category/${title}`}>{title}</Link>
         <MdKeyboardArrowRight
-          className="dropdown-item-icon"
+          className='dropdown-item-icon'
           onClick={() => setOpen(!open)}
         />
       </li>
       {open &&
-        categories&& categories.subcategories?.map((data, index) => {
-          
+        categories &&
+        categories.subcategories?.map((data, index) => {
           return (
             <>
-            <ul className="subdropdown" key={index}>
-              <li className="navar-dropdown-li" ref={myRef} onClick={handleClickInside}>
-                <Link to={`/${title}`} state={data.name} key={index}>{data?.name}</Link>
-              </li>
-            </ul>
+              <ul className='subdropdown' key={index}>
+                <li
+                  className='navar-dropdown-li'
+                  ref={myRef}
+                  onClick={handleClickInside}
+                >
+                  <Link to={`/category/${title}`} state={data.name} key={index}>
+                    {data?.name}
+                  </Link>
+                </li>
+              </ul>
             </>
           );
-        })
-        }
+        })}
     </>
   );
 };
